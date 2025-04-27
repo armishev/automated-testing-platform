7453c7641b580249b215b18a10833543e56682c1
armishev <kirillarmishev@gmail.com>
2025-04-27 17:48:40 +0300

    1

--- telegram-bot/src/main/java/com/armishev/tvm/telegrambot/TelegramBot.java
+++ telegram-bot/src/main/java/com/armishev/tvm/telegrambot/TelegramBot.java
@@ -1,9 +1,15 @@
 package com.armishev.tvm.telegrambot;
 
+import java.io.ByteArrayInputStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,6 +25,9 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Value("${telegram.bot.username}")
     private String botUsername;
+
+    @Value("${imagerenderer.url}")
+    private String imageRendererUrl;
 
     public TelegramBot(@Value("${telegram.bot.token}") String botToken) {
         super(botToken);
@@ -41,33 +50,75 @@ public class TelegramBot extends TelegramLongPollingBot {
             switch (messageText.toLowerCase()) {
                 case "/start":
                     response = "Привет! Я бот для автоматизированного тестирования. Введите /help для получения списка команд.";
+                    sendText(chatId, response);
                     break;
                 case "/help":
                     response = "Доступные команды:\n" +
                             "/start - начать работу с ботом\n" +
                             "/help - список команд\n" +
                             "/test - выполнить тестовую команду";
+                    sendText(chatId, response);
                     break;
                 case "/test":
                     response = "Тестовая команда выполнена!"; // Здесь можно добавить вызов сервиса или другую логику
+                    sendText(chatId, response);
+                    break;
+                case "/metrics":
+                    try {
+                        byte[] imageBytes = getScreenshot("http://147.45.150.56:4000/public-dashboards/9191b094754e459688fa1aaeecb77794 "); // Или получаем URL из сообщения
+                        sendPhoto(chatId, imageBytes);
+                    } catch (Exception e) {
+                        logger.error("Ошибка при получении скриншота: {}", e.getMessage());
+                        sendText(chatId, "Произошла ошибка при создании скриншота.");
+                    }
                     break;
                 default:
                     response = "Неизвестная команда. Введите /help для списка доступных команд.";
+                    sendText(chatId, response);
                     break;
             }
+        }
+    }
 
-            SendMessage message = new SendMessage();
-            message.setChatId(chatId.toString());
-            message.setText(response);
+    // Метод для отправки текстового сообщения
+    private void sendText(Long chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId.toString());
+        message.setText(text);
+        try {
+            execute(message);
+            logger.info("Отправлено сообщение для chatId {}: {}", chatId, text);
+        } catch (TelegramApiException e) {
+            logger.error("Ошибка при отправке сообщения: {}", e.getMessage());
+        }
+    }
 
-            try {
-                execute(message);
-                // Логирование успешной отправки сообщения
-                logger.info("Отправлено сообщение для chatId {}: {}", chatId, response);
-            } catch (TelegramApiException e) {
-                logger.error("Ошибка при отправке сообщения для chatId {}: {}", chatId, e.getMessage());
-                e.printStackTrace();
-            }
+    // Метод для получения скриншота от сервиса image-renderer
+    private byte[] getScreenshot(String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        String requestUrl = UriComponentsBuilder
+                .fromHttpUrl(imageRendererUrl)
+                .path("/api/render")
+                .queryParam("url", url)
+                .build()
+                .toUriString();
+        // Формируем URL запроса к image-renderer, например:
+        logger.info("requestUrl: {}", requestUrl);
+        // Отправляем GET запрос, ожидаем byte[]
+        return restTemplate.getForObject(requestUrl, byte[].class);
+    }
+
+    // Метод для отправки фотографии в чат
+    private void sendPhoto(Long chatId, byte[] imageBytes) {
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId.toString());
+        // Оборачиваем байты в InputStream и задаем имя файла
+        sendPhoto.setPhoto(new InputFile(new ByteArrayInputStream(imageBytes), "screenshot.png"));
+        try {
+            execute(sendPhoto);
+            logger.info("Отправлена фотография для chatId {}", chatId);
+        } catch (TelegramApiException e) {
+            logger.error("Ошибка при отправке фотографии: {}", e.getMessage());
         }
     }
 }
